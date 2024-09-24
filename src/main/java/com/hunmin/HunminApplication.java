d63f1332078343a529997cdda067b6bcfdde74ad
yejin <yejin331@daum.net>
2024-09-24 15:09:47 +0900

    [chore] 프로젝트 세팅

---
+++ src/main/java/com/hunmin/HunminApplication.java
@@ -0,0 +1,13 @@
+package com.hunmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HunminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hunmin/HunminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
