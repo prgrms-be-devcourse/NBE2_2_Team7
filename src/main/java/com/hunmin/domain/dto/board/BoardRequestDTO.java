24696e126f7cc1bbbcc17847b313736ffa009815
yejin <yejin331@daum.net>
2024-09-26 21:49:30 +0900

    [mod] 어노테이션 추가

--- src/main/java/com/hunmin/domain/dto/board/BoardRequestDTO.java
+++ src/main/java/com/hunmin/domain/dto/board/BoardRequestDTO.java
@@ -1,15 +1,29 @@
 package com.hunmin.domain.dto.board;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BoardRequestDTO {
+    @NotBlank
     private Long boardId;
+
+    @NotBlank
     private Long memberId;
+
+    @NotEmpty
     private String title;
+
+    @NotEmpty
     private String content;
+
     private String location;
     private Double latitude;
     private Double longitude;
