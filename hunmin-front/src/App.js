a1c55f5adacd203d0f30050e9c907f371050a304
DongWooKim4343 <kd6904@naver.com>
2024-09-27 19:21:32 +0900

    [feat] 공지사항 프론트 구현

--- hunmin-front/src/App.js
+++ hunmin-front/src/App.js
@@ -3,16 +3,28 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BoardListPage from './board/BoardListPage';
 import BoardDetailPage from './board/BoardDetailPage';
 import CreateBoardPage from './board/CreateBoardPage';
+import NoticeListPage from './notice/NoticeListPage';
+import NoticeDetailPage from './notice/NoticeDetailPage';
+import CreateNoticePage from './notice/CreateNoticePage';
 
 const App = () => {
     return (
         <Router>
             <Routes>
+                {/* 게시판 라우트 */}
                 <Route path="/" element={<BoardListPage />} />
                 <Route path="/board/:boardId" element={<BoardDetailPage />} />
                 <Route path="/create-board" element={<CreateBoardPage />} />
+
+                {/* 공지사항 라우트 */}
+                <Route path="/notices" element={<NoticeListPage />} />
+                <Route path="/notices/:id" element={<NoticeDetailPage />} />
+                <Route path="/create-notice" element={<CreateNoticePage />} />
+                <Route path="/edit-notice/:id" element={<CreateNoticePage />} />
+
             </Routes>
         </Router>
+
     );
 };
 
