c51ebf7dae3c465d230e999cfebde99bd617e6ee
newbie_all_the_time <tupac52@gmail.com>
2024-09-27 11:34:36 +0900

    [feat] build.gradle 수정, MemberRepositoryTest 수정

--- src/test/java/com/hunmin/domain/repository/MemberRepositoryTest.java
+++ src/test/java/com/hunmin/domain/repository/MemberRepositoryTest.java
@@ -5,7 +5,6 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
