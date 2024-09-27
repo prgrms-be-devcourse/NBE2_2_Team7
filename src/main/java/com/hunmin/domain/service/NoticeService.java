89366b6aba20e9587cc7b8ffd47403fc1cca7af9
DongWooKim4343 <kd6904@naver.com>
2024-09-27 15:33:03 +0900

    [feat] 공지사항 컨트롤러 구현

--- src/main/java/com/hunmin/domain/service/NoticeService.java
+++ src/main/java/com/hunmin/domain/service/NoticeService.java
@@ -1,10 +1,11 @@
 package com.hunmin.domain.service;
 
 import com.hunmin.domain.dto.notice.*;
-import com.hunmin.domain.dto.page.PageRequestDTO;
+import com.hunmin.domain.entity.Member;
 import com.hunmin.domain.entity.MemberRole;
 import com.hunmin.domain.entity.Notice;
 import com.hunmin.domain.exception.NoticeException;
+import com.hunmin.domain.repository.MemberRepository;
 import com.hunmin.domain.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -22,6 +23,7 @@ import java.util.List;
 @Log4j2
 public class NoticeService {
     private final NoticeRepository noticeRepository;
+    private final MemberRepository memberRepository;
 
     //공지사항 리스트 조회
     public List<NoticeResponseDTO> getAllNotices(NoticePageRequestDTO pageRequestDTO){
@@ -35,7 +37,7 @@ public class NoticeService {
             }
             return responseDTOs;
         }catch (Exception e){
-            log.error("getAllNotices error: {}",  e);
+            log.error("getAllNotices error: {}",  e.getMessage());
             throw NoticeException.NOTICE_NOT_FOUND.get();
         }
     }
@@ -48,24 +50,28 @@ public class NoticeService {
 
     //공지사항 등록
     public NoticeResponseDTO createNotice(NoticeRequestDTO noticeRequestDTO){
+        Member member = getMember(noticeRequestDTO.getMemberId());
+
         //관리자 아닐경우 예외 발생
-        if (!noticeRequestDTO.getMember().getMemberRole().equals(MemberRole.ADMIN)) {
+        if (!member.getMemberRole().equals(MemberRole.ADMIN)) {
             throw NoticeException.MEMBER_NOT_VALID.get();
         }
             try {
-                Notice notice = noticeRequestDTO.toEntity();
+                Notice notice = noticeRequestDTO.toEntity(member);
                 Notice savedNotice = noticeRepository.save(notice);
+                log.info("Notice created successfully. Notice ID: {}", savedNotice.getNoticeId());
                 return new NoticeResponseDTO(savedNotice);
             }catch (Exception e) {
-                log.error("createNotice error: {}",  e);
+                log.error("createNotice error: {}",  e.getMessage());
                 throw NoticeException.NOTICE_NOT_CREATED.get();
             }
     }
 
     //공지사항 수정
     public NoticeResponseDTO updateNotice(NoticeUpdateDTO noticeUpdateDTO){
+        Member member = getMember(noticeUpdateDTO.getMemberId());
         //관리자 아닐경우 예외 발생
-        if (!noticeUpdateDTO.getMember().getMemberRole().equals(MemberRole.ADMIN)) {
+        if (!member.getMemberRole().equals(MemberRole.ADMIN)) {
             throw NoticeException.MEMBER_NOT_VALID.get();
         }
         Notice notice = noticeRepository.findById(noticeUpdateDTO.getNoticeId()).orElseThrow(NoticeException.NOTICE_NOT_FOUND::get);
@@ -73,29 +79,33 @@ public class NoticeService {
         try{
             notice.changeTitle(noticeUpdateDTO.getTitle());
             notice.changeContent(noticeUpdateDTO.getContent());
-            notice.changeMember(noticeUpdateDTO.getMember());
             return new NoticeResponseDTO(notice);
         }catch (Exception e){
-            log.error("updateNotice error: {}",  e);
+            log.error("updateNotice error: {}",  e.getMessage());
             throw NoticeException.NOTICE_NOT_UPDATED.get();
         }
     }
 
+
+
     //공지사항 삭제
-    public NoticeResponseDTO deleteNotice(NoticeDeleteDTO noticeDeleteDTO){
+    public boolean deleteNotice(NoticeDeleteDTO noticeDeleteDTO){
+        Member member = getMember(noticeDeleteDTO.getMemberId());
         //관리자 아닐경우 예외 발생
-        if (!noticeDeleteDTO.getMember().getMemberRole().equals(MemberRole.ADMIN)) {
+        if (!member.getMemberRole().equals(MemberRole.ADMIN)) {
             throw NoticeException.MEMBER_NOT_VALID.get();
         }
-        Notice notice = noticeRepository.findById(noticeDeleteDTO.getNoticeId()).orElseThrow(NoticeException.NOTICE_NOT_FOUND::get);
+        noticeRepository.findById(noticeDeleteDTO.getNoticeId()).orElseThrow(NoticeException.NOTICE_NOT_FOUND::get);
         try {
             noticeRepository.deleteById(noticeDeleteDTO.getNoticeId());
-            return new NoticeResponseDTO(notice);
+            return true;
         }catch (Exception e) {
-            log.error("deleteNotice error: {}",  e);
+            log.error("deleteNotice error: {}",  e.getMessage());
             throw NoticeException.NOTICE_NOT_DELETED.get();
         }
     }
 
-
+    private Member getMember(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new RuntimeException("Member not found"));
+    }
 }
