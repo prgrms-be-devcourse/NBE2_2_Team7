1ce2267f26d99f98d9c965dc58ee8ae2d17fb16b
yejin <yejin331@daum.net>
2024-09-30 12:12:48 +0900

    [feat] 댓글, 대댓글 알림 기능 구현

--- src/main/java/com/hunmin/domain/controller/advice/APIControllerAdvice.java
+++ src/main/java/com/hunmin/domain/controller/advice/APIControllerAdvice.java
@@ -2,6 +2,7 @@ package com.hunmin.domain.controller.advice;
 
 import com.hunmin.domain.exception.BoardTaskException;
 import com.hunmin.domain.exception.CommentTaskException;
+import com.hunmin.domain.exception.NotificationTaskException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,6 +22,14 @@ public class APIControllerAdvice {
     //댓글 예외 처리
     @ExceptionHandler(CommentTaskException.class)
     public ResponseEntity<Map<String, String>> handleCommentTaskException(CommentTaskException e) {
+        Map<String, String> map = Map.of("error", e.getMessage());
+
+        return ResponseEntity.status(e.getCode()).body(map);
+    }
+
+    //알림 예외 처리
+    @ExceptionHandler(NotificationTaskException.class)
+    public ResponseEntity<Map<String, String>> handleNotificationTaskException(NotificationTaskException e) {
         Map<String, String> map = Map.of("error", e.getMessage());
 
         return ResponseEntity.status(e.getCode()).body(map);
