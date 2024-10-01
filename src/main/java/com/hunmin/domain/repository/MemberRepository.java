135b2cc45becda376fb035cee05eaea753f6df81
yejin <yejin331@daum.net>
2024-10-01 18:09:37 +0900

    [merge] merge develop

--- src/main/java/com/hunmin/domain/repository/MemberRepository.java
+++ src/main/java/com/hunmin/domain/repository/MemberRepository.java
@@ -2,7 +2,9 @@ package com.hunmin.domain.repository;
 
 import com.hunmin.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     
     // 회원 정보 조회
