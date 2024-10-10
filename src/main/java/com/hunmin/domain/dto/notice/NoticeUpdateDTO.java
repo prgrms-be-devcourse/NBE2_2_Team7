fd24e3b62c049d30cbb264081a9270ced46a6980
DongWooKim4343 <kd6904@naver.com>
2024-10-10 11:24:50 +0900

    [fix] 공지 등록, 변경 수정

--- src/main/java/com/hunmin/domain/dto/notice/NoticeUpdateDTO.java
+++ src/main/java/com/hunmin/domain/dto/notice/NoticeUpdateDTO.java
@@ -1,5 +1,6 @@
 package com.hunmin.domain.dto.notice;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,15 +9,18 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+/* memberId는 SecurityContext 에서 받아오고 noticeId는 path variable 에서 받아오는데 필요하지 않아서 다 무시
+나머지 불필요한 JSON 데이터 무시*/
 public class NoticeUpdateDTO {
-/*
-RequestDTO와 인스턴스가 같긴 한데 용도는 다르니 혹시몰라서 ..
- */
+
 
     @NotEmpty(message = "제목을 입력하세요")
     private String title;
     @NotEmpty(message = "내용을 입력하세요")
     private String content;
+
+
 
 
 
